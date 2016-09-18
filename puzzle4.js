b237db0b7d32d6826b109e3508abc584bc33deb5
linzi9205 <lwu@cca.edu>
2016-09-18 12:24:24 -0700

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,35 @@
+// Help Rudy the Red Dot collect all the green dots!
+
+// Control Rudy with the following commands:
+//  up();                   -- moves Rudy up one square
+//  down();                 -- ...down one square
+//  left();                 -- ...left one square
+//  right();                -- ...right one square
+
+// Interact with Rudy's environment using the following commands:
+//  remainingDots();        -- returns the number of green dots remaining
+//  getColor();             -- returns the color of Rudy's square
+//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
+
+
+function go(n,direction){
+  var steps=0;
+  while(steps<n){
+    direction();
+    steps=steps+1;
+  }
+}
+
+
+go(4,down);
+go(1,right);
+var color=getColor();
+go(1,down);
+color=setColor(color);
+go(2,right);
+go(1,up);
+go(1,right);
+color=getColor();
+go(1,down);
+color=setColor(color);
+go(2,right);
